07ff88991abd757013754d9c2c9d38fe50500b9a
stal2k <stal2k@gmail.com>
2015-08-26 18:21:41 -0400

    Initial commit

---
+++ mobile/src/main/java/com/example/organiclessons/gtek/organiclesson/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.organiclessons.gtek.organiclesson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+// Enable Javascript
+        //WebSettings webSettings = mWebView.getSettings();
+        //webSettings.setJavaScriptEnabled(true);
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        mWebView.loadUrl("http://www.organiclesson.com/");
+    }
+/* http://developer.android.com/guide/webapps/webview.html good resource */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
